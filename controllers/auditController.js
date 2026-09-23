ec97bdfea3b2bc7554ada1a5b0f949b024330612
phamson333zzz-sudo <phamson333zzz@gmail.com>
2026-09-23 23:59:59 +0700

    LayLichSuThaoTac_phamvanson

---
+++ controllers/auditController.js
@@ -0,0 +1,14 @@
+﻿const { AuditLog, User } = require('../models');
+
+// Lay lich su thao tac he thong (Audit Trail)
+exports.getAuditLogs = async (req, res) => {
+  try {
+    const logs = await AuditLog.findAll({
+      order: [['id', 'DESC']],
+      limit: 100
+    });
+    res.json({ success: true, logs });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+};
