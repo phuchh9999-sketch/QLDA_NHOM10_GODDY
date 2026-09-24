e496f19a932ee2b59b0d01e0b93255131a17cd9b
phamson333zzz-sudo <phamson333zzz@gmail.com>
2026-09-24 14:24:12 +0700

    APIQuanLyCongNo_phamvanson

---
+++ routes/debtRoutes.js
@@ -0,0 +1,9 @@
+﻿const express = require('express');
+const router = express.Router();
+const debtController = require('../controllers/debtController');
+
+router.get('/overview', debtController.getDebtOverview);
+router.post('/payment', debtController.recordPayment);
+router.post('/remind', debtController.sendDebtReminder);
+
+module.exports = router;
