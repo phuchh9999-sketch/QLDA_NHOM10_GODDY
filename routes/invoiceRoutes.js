37d352ebf28555cdde5f500121f15ad1636ca92d
phamson333zzz-sudo <phamson333zzz@gmail.com>
2026-09-24 14:24:39 +0700

    APIQuanLyHoaDon_phamvanson

---
+++ routes/invoiceRoutes.js
@@ -0,0 +1,10 @@
+﻿const express = require('express');
+const router = express.Router();
+const invoiceController = require('../controllers/invoiceController');
+
+router.get('/', invoiceController.getInvoices);
+router.get('/:id', invoiceController.getInvoiceById);
+router.post('/from-placement', invoiceController.createInvoiceFromPlacement);
+router.put('/:id/cancel', invoiceController.cancelInvoice);
+
+module.exports = router;
