02a45d6ab34e7062cf50ace127786340c89cea8b
phamson333zzz-sudo <phamson333zzz@gmail.com>
2026-09-24 14:25:22 +0700

    APITuyenDung_phamvanson

---
+++ routes/recruitmentRoutes.js
@@ -0,0 +1,18 @@
+﻿const express = require('express');
+const router = express.Router();
+const recruitmentController = require('../controllers/recruitmentController');
+
+// Jobs
+router.get('/jobs', recruitmentController.getJobs);
+router.post('/jobs', recruitmentController.createJob);
+router.put('/jobs/:id', recruitmentController.updateJob);
+
+// Candidates
+router.get('/candidates', recruitmentController.getCandidates);
+router.post('/candidates', recruitmentController.createCandidate);
+
+// Placements
+router.get('/placements', recruitmentController.getPlacements);
+router.post('/placements', recruitmentController.createPlacement);
+
+module.exports = router;
