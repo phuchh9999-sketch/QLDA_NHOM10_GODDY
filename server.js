d114c89c235059902a6d2dc8e3fc755f354db613
Huynh Nguyen Vinh Phuc <phuchh9999@gmail.com>
2026-09-24 11:18:31 +0700

    CapNhatServerJs_huynhnguyenvinhphuc

---
+++ server.js
@@ -0,0 +1,11 @@
+﻿const express = require('express');
+const cors = require('cors');
+const path = require('path');
+require('dotenv').config();
+
+// Khởi tạo máy chủ ứng dụng GODDY Recruit
+const { sequelize } = require('./models');
+const seedData = require('./config/seed');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
